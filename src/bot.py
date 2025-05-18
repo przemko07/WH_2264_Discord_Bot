901c4cbaa1168cb4d27550c457ba800ea063d67f
Przemko07 <theprzemko07@gmail.com>
2025-05-18 11:52:29 +0200

    role receiving works. I just needed to add "intents" in admin dashboard

--- src/bot.py
+++ src/bot.py
@@ -2,37 +2,72 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import helpers
+
+# ---------- const -----------
+
+NEW_COMMER_ROLE_ID = 1373321252160409610    # Role ID for new commers (receiving this role triggers functions)
+BOT_LOG_CH_ID = 1373323014535381034         # Text channel for writing bot logs
+
+
 
 # ---------- config ----------
 load_dotenv()
 TOKEN   = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True  # needed for prefix commands
+intents.message_content = True          # needed for prefix commands
+intents.members = True                  # lets us receive member-update events
 
 # ---------- prefix commands ----------
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
+    print("[info] called `on_ready`")
     print(f"✅ Logged in as {bot.user} (ID {bot.user.id})")
+
+
+@bot.event
+async def on_member_update(before: discord.Member, after: discord.Member):
+    print("[info] called `on_member_update`")
+    log_channel = after.guild.get_channel(BOT_LOG_CH_ID)
+    if log_channel == None:
+        raise RuntimeError("channel == None")
+    await log_channel.send("[info] called `on_member_update`")
+
+    # Roles are *lists* that preserve hierarchy order.
+    added_roles   = [r for r in after.roles if r not in before.roles]
+    removed_roles = [r for r in before.roles if r not in after.roles]
+
+    for role in added_roles:
+        if role.id == NEW_COMMER_ROLE_ID:
+            await log_channel.send(f"✅ {after.mention} just received **{role.name}**")
 
 @bot.command()
 async def ping(ctx: commands.Context):
+    print("[info] called `ping`")
     """Replies with Pong!"""
     await ctx.reply("🏓 Pong!", mention_author=False)
 
 # ---------- slash commands ----------
 @bot.tree.command(name="hello", description="Say hello back!")
 async def hello(interaction: discord.Interaction):
+    print("[info] called `hello`")
+    channel = interaction.guild.get_channel(BOT_LOG_CH_ID)
+    if channel == None: raise RuntimeError("channel == None")
+    await channel.send("raised `hello`")
+
     await interaction.response.send_message(
         f"👋 Hey {interaction.user.mention}!"
     )
 
+
 # make sure slash commands are pushed on startup
 @bot.event
 async def setup_hook():
+    print("[info] called `setup_hook`")
     await bot.tree.sync()  # global sync; use guild-specific in dev for instant sync
 
-# ---------- kick it off ----------
+# ---------- start bot ----------
 bot.run(TOKEN)
